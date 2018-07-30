3e4dbcc683510f156f4dd67ed01ab4a6827cd40a
Luis Angel Mojena Román <lamojena@estudiantes.uci.cu>
2018-07-30 18:25:20 +0000

    REFACTOR: Change the validation module so it does not have side effects. - Added the add_information_message function. - Added documentation to validators class. - Refactor the validate methods to remove side effects (modifying g).

--- alayatodo/validators.py
+++ alayatodo/validators.py
@@ -10,28 +10,42 @@ def add_validation_message(message):
     g.messages.append({'text': message, 'type': MessageType.Error})
 
 
+def add_information_message(message):
+    g.messages.append({'text': message, 'type': MessageType.Information})
+
+
 class validators:
+    """
+    All functions in this class return a tuple containing two values
+    in the following form.
+    >>> valid, message = validate_not_empty('abc')
+
+    valid - Specifies if the value to be validated results valid,
+    in which case the returned value will be True and False otherwise
+
+    message - Holds a message to be displayed if the value passed in
+    is not valid. If the value is valid returns None.
+
+    """
 
     @staticmethod
     def validate_not_empty(value):
+        message = 'Required field with empty value.'
         if value is not None:
             if value.strip() != '':
-                return value
+                return True, None
             else:
-                add_validation_message('Required field with empty value.')
-                return None
+                return False, message
         else:
-            add_validation_message('Required field with empty value.')
-            return None
+            return False, message
 
     @staticmethod
     def validate_not_empty_field(value, field):
+        message = 'Required field: \'%s\' with empty value.' % field
         if value is not None:
             if value.strip() != '':
-                return value
+                return True
             else:
-                add_validation_message('Required field: \'%s\' with empty value.' % field)
-                return None
+                return False, message
         else:
-            add_validation_message('Required field: \'%s\' with empty value.' % field)
-            return None
+            return False, message
